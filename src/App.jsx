70f221685046593a90220d3214c005669193622d
wiwatle <tonn_lephon1@hotmail.com>
2026-03-01 03:07:51 +0700

    19 commit use swr api

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import useSWR from 'swr';
+
+
+
+
 function App() {
   const [count, setCount] = useState(0)
-
+  
+  const fetcher = (url) => fetch(url).then((res) => res.json());
+  const { data, error } = useSWR('https://jsonplaceholder.typicode.com/users', fetcher);
+    if (error) return <p>Error loading data</p>;
+    if (!data) return <p>Loading...</p>;
+ 
+  
   return (
     <>
       <div>
@@ -17,6 +28,7 @@ function App() {
         </a>
       </div>
       <h1>wiwat lephon 009</h1>
+      {/*
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -25,9 +37,24 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+     
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+       */}
+        
+        <div>
+            <ul>
+                {data.map((user) => (
+                    <li key={user.id}>
+                        <h3>{user.username}</h3>
+                        <p>{user.name}</p>
+                        <p>{user.email}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+
     </>
   )
 }
